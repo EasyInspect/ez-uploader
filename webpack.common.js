c4a95029850a4709c853031dfc00426c66ea4a57
mfn <begger-008@hotmail.com>
2018-06-06 17:20:09 +0200

    remove babel-polyfill

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path              = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
+    entry: ['./src/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "index.js",
